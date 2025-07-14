96778a41189f7e87fd8608fab1871bcd46a81df7
Андрей Зорькин <a1handreay@gmail.com>
2025-07-14 15:21:21 +0300

    add Internal Player struct

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/a1ndreay/football-analitics
+
+go 1.24.4
---
+++ internal/player/player.go
@@ -0,0 +1,43 @@
+package player
+
+import "slices"
+
+type Player struct {
+	Name    string
+	Goals   int
+	Misses  int
+	Assists int
+	Rating  float64
+}
+
+func calculateRating(goals, misses, assists int) float64 {
+	var r float64
+	switch {
+	case misses == 0:
+		r = float64(goals + assists/2)
+	default:
+		r = float64((goals + assists/2)) / float64(misses)
+	}
+	return r
+}
+
+func NewPlayer(name string, goals, misses, assists int) Player {
+	r := calculateRating(goals, misses, assists)
+	return Player{Name: name, Goals: goals, Misses: misses, Assists: assists, Rating: r}
+}
+
+func goalsSort(players []Player) []Player {
+	slices.SortFunc(players, byGoalsDesc)
+	return players
+}
+
+func byGoalsDesc(a, e Player) int {
+	switch {
+	case a.Goals < e.Goals:
+		return -1
+	case a.Goals > e.Goals:
+		return 1
+	default:
+		return 0
+	}
+}
